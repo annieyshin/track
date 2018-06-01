b03ea25d2bba661aa92613a8cc8dc12f07803a7b
Annie Shin <annieyshin@gmail.com>
2018-06-01 15:38:53 -0700

    fix the Js if else statements, in theory

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,7 @@
 
 // USER INTERFACE
 $(document).ready(function() {
-  // event.preventDefault();
-  // $("#nameinput").submit(function() {
-  //   $(".name1").text(varname);
-  // });
+
   var motivation = confirm("If you are interested in the full time program, would you feel comfortable spending 40 hours a week Monday through Friday? Click OK for yes or Cancel for no.");
 
     if (motivation) {
@@ -14,30 +11,29 @@ $(document).ready(function() {
         $('#not-motivated').show();
     }
 
+$("form#choice").click(function(event) {
+  console.log("reached");
+    event.preventDefault();
+    console.log("reached");
+  var cssreact = $("select#csschoice").val();
+  var rubyrails = $("select#rubychoice").val();
+  var cnet = $("select#cnetchoice").val();
 
+    if (cssreact === 'Yes')
+      {
+        $('#CSS-box').show();
+      }
+      else if (rubyrails === 'Yes')
+      {
+        $('#Ruby-box').show();
+      }
+      else  if (cnet === 'Yes')
+      {
+        $('#CNET-box').show();
+      }
+      else {
 
-
+      }
 
   });
-
-  // $("form#sexual").submit(function() {
-  //   var sex = $("select#sexualpick").val();
-  //
-  //     if (sex === 'gay') {
-  //       $('#gaybox').show();
-  //     } else {
-  //         $('#straightbox').show();
-  //     }
-// $(document).ready(function() {
-  // $("#submit").click(function(event) {
-  //   event.preventDefault();
-//     var varfoods = parseInt($("#foods").val());
-//     var varmusic = parseInt($("#music").val());
-//     var varcolor = parseInt($("#color").val());
-//     var varanimal = parseInt($("#animal").val());
-//     var varmovie = parseInt($("#movie").val());
-//     var varbook = parseInt($("#book").val());
-//     var result = varfoods + varmusic + varcolor + varanimal + varmovie + varbook;
-//     console.log(varfoods);
-//     $("#output").text(result);
-//     });
+});
